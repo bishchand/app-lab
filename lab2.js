9bcaa9a2b612f99083f51a6d8683f69df0c5149d
bishchand <32349708+bishchand@users.noreply.github.com>
2018-09-19 09:59:58 +0800

    Update lab2.js

--- lab2.js
+++ lab2.js
@@ -1,16 +1,19 @@
 let waitingTime = 0;
-let holdingPhone = input("is the customer holding a phone? yes/no");
-let isAlone = input("is the customer alone? yes/no");
+let holdingPhone = console.log("is the customer holding a phone? yes/no");
+let isAlone = console.log("is the customer alone? yes/no");
 
 if holdingPhone == "yes";
     waitingTime =+ 1;
 else if holdingPhone == "no";
     waitingTime =+ 3;
-else print("Invalid answer. Please write yes or no.");
+else 
+    console.log("Invalid answer. Please write yes or no.");
 
 if isAlone == "no";
     waitingTime =+ 2;
 else if isAlone == "yes";
     waitingTime =+ 0.5;
+else 
+    console.log("Invalid answer. Please write yes or no.");
 
-print("Estimated drink time: ", waitingTime)
+console.log("Estimated drink time: ", waitingTime)

c01ca8ea33e818800d04168b6976fdc141cbea8f
bishchand <32349708+bishchand@users.noreply.github.com>
2018-12-14 11:28:29 +0800

    Create challenge1.js

---
+++ lab3/challenge1.js
@@ -0,0 +1,12 @@
+function reverseString(str) {
+    
+    var splitString = str.split(``);
+
+    var reverseArray = splitString.reverse(``);
+
+    var joinArray = reverseArray.join(``);
+
+    return joinArray;
+
+}
+console.log (reverseString(`gniht eht si desrever racecar`));
